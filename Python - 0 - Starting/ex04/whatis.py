d1a5e444e2e962561ffa81145f4af210fe2a5a72
osamabel <osamaalpha1997@gmail.com>
2025-10-26 12:55:10 +0100

    P0-ex04

--- Python - 0 - Starting/ex04/whatis.py
+++ Python - 0 - Starting/ex04/whatis.py
@@ -1,26 +1,18 @@
 import sys
 
-def check_odd_or_even():
-    # Check number of arguments
-    if len(sys.argv) == 1:
-        # No arguments provided, do nothing
-        return
-    elif len(sys.argv) > 2:
-        # More than one argument
-        print("AssertionError: more than one argument is provided")
-        return
-    
+# Check number of arguments
+if len(sys.argv) > 2:
+    # More than one argument
+    print("AssertionError: more than one argument is provided")
+elif len(sys.argv) == 2:
     # Try to convert to integer
     try:
         number = int(sys.argv[1])
     except ValueError:
         print("AssertionError: argument is not an integer")
-        return
-    
-    # Check if odd or even
-    if number % 2 == 0:
-        print("I'm Even.")
     else:
-        print("I'm Odd.")
-
-check_odd_or_even()
+        # Check if odd or even
+        if number % 2 == 0:
+            print("I'm Even.")
+        else:
+            print("I'm Odd.")
